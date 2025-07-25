46335a87cd24e7f440049e9a6cc1cd854b681d15
Wiktor Jeryś <SDanrejk@gmail.com>
2025-07-25 16:03:53 +0200

    mockups

---
+++ call_for_action/actions.js
@@ -0,0 +1,42 @@
+var callForActions = [
+    {
+        type: 'Workshop',
+        name: 'Berlin Environment Hack',
+        organizer: 'Eco Berlin',
+        dateStart: '2025-08-01',
+        dateEnd: '2025-08-02',
+        description: '24-hour team coding and action for novel clean air solutions in Berlin.',
+        lat: 52.52,
+        lng: 13.405
+    },
+    {
+        type: 'Protest',
+        name: 'Munich Climate March',
+        organizer: 'Green Steps',
+        dateStart: '2025-08-10',
+        dateEnd: '2025-08-10',
+        description: 'Citizens unite in Munich\'s downtown for climate justice.',
+        lat: 48.137,
+        lng: 11.576
+    },
+    {
+        type: 'Seminar',
+        name: 'Hamburg Water Future',
+        organizer: 'Blue Planet Foundation',
+        dateStart: '2025-09-05',
+        dateEnd: '2025-09-05',
+        description: 'Expert talks and workshops on innovative water conservation.',
+        lat: 53.551,
+        lng: 9.994
+    },
+    {
+        type: 'Cleanup',
+        name: 'Frankfurt River Day',
+        organizer: 'Clean Main',
+        dateStart: '2025-08-28',
+        dateEnd: '2025-08-28',
+        description: 'Join local volunteers and keep the Main riverbanks trash-free!',
+        lat: 50.1109,
+        lng: 8.6821
+    }
+];
