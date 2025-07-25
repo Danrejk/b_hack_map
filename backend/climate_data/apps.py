7409262860d4408d092758de7fb301816bd35b2e
Bhanu Prasanna <bhanu.prasanna2001@gmail.com>
2025-07-25 17:56:01 +0200

    Update

--- backend/climate_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClimateDataConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "climate_data"
--- backend/climate_data/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("points/", views.ClimateDataPointListView.as_view(), name="climate_data_points"),
-    path("risk-areas/", views.ClimateRiskAreaListView.as_view(), name="climate_risk_areas"),
-    path("weather/", views.WeatherDataListView.as_view(), name="weather_data"),
-    path("map-data/", views.map_data_view, name="map_data"),
-]
